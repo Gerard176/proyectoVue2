f2f3b507471eba6ce91140282acdba322136d7ce
Nicolas <gaduarter@ufpso.edu.co>
2023-05-20 12:05:45 -0500

    directivas

---
+++ proyectovue2/src/App.vue
@@ -0,0 +1,36 @@
+<script setup>
+
+  import { ref, computed} from 'vue'
+  const users = [
+    {
+      name: 'pepito',
+      lastname: 'perez',
+      
+    },
+    {
+      name: 'juliana',
+      lastname: 'perez',
+      age: '10000'
+    }
+
+  ]
+  const num = ref(2)
+  const computado =  computed(function(){
+    return num.value > 0 ? num.value*3 : 0
+  })
+</script>
+
+<template>
+  Valor computado: {{ computado }}
+  <div v-for="(user,index) in users" :key="index"> 
+  Nombre completo: {{ user.name }} {{ user.lastname }}
+  <span v-if="user.age">
+    Edad: {{ user.age }}
+  </span>
+  </div>
+</template>
+
+<style scoped>
+
+
+</style>
